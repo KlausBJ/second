6fc69c86047ec172b79ab8826ff39e1d8753f7ab
Klaus Bech Jensen <klaus.bech.jensen@gmail.com>
2018-11-08 11:22:19 +0100

    added app_versions_controller and db_versions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'app_versions/new'
+
+  get 'app_versions/index'
+
+  get 'app_versions/edit'
+
+  get 'app_versions/update'
+
+  get 'app_versions/delete'
+
+  get 'app_versions/next'
+
+  get 'app_versions/clone'
+
+  get 'db_versions/new'
+
+  get 'db_versions/index'
+
+  get 'db_versions/edit'
+
+  get 'db_versions/update'
+
+  get 'db_versions/delete'
+
+  get 'db_versions/next'
+
+  get 'db_versions/clone'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
